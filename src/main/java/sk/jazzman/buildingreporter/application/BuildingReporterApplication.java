eba4bfe8d7dac91f95fc3293d5c606ba3e428534
jano <kovalcikj@gmail.com>
2013-11-15 21:02:37 +0100

    config appl

--- src/main/java/sk/jazzman/buildingreporter/application/BuildingReporterApplication.java
+++ src/main/java/sk/jazzman/buildingreporter/application/BuildingReporterApplication.java
@@ -9,6 +9,7 @@ import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,7 @@ public class BuildingReporterApplication extends WebApplication implements Seria
 	/** Serial id */
 	private static final long serialVersionUID = 1L;
 
+	@Autowired
 	private ApplicationContext applicationContext;
 
 	@Override
@@ -32,8 +34,20 @@ public class BuildingReporterApplication extends WebApplication implements Seria
 
 		getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext, true));
 
+		getMarkupSettings().setDefaultMarkupEncoding("UTF-8");
+        getRequestCycleSettings().setResponseRequestEncoding("UTF-8");
+ 
+                  getMarkupSettings().setStripWicketTags(true);
+            getMarkupSettings().setStripComments(true);
+            getMarkupSettings().setCompressWhitespace(true);
+        
 	}
-
+	
+	/**
+	 * Application getter
+	 * 
+	 * @return
+	 */
 	public static BuildingReporterApplication get() {
 		return (BuildingReporterApplication) WebApplication.get();
 	}
